07d41c4ce1f994477544a340cebd802083b27df0
ToniCreswell <ac2211@ic.ac.uk>
2018-01-11 18:04:25 +0000

    update default args

--- scripts/dcgan_celebA.py
+++ scripts/dcgan_celebA.py
@@ -40,8 +40,8 @@ def get_args():
 	parser.add_argument('--root', default='../../../../data/', type=str)
 	parser.add_argument('--batchSize', default=64, type=int)
 	parser.add_argument('--maxEpochs', default=10, type=int)
-	parser.add_argument('--nz', default=200, type=int)
-	parser.add_argument('--lr', default=2e-3, type=float)
+	parser.add_argument('--nz', default=100, type=int)
+	parser.add_argument('--lr', default=1e-3, type=float)
 	parser.add_argument('--fSize', default=64, type=int)  #multiple of filters to use
 	parser.add_argument('--outDir', default='../../Experiments/', type=str)
 	parser.add_argument('--commit', required=True, type=str)
@@ -53,7 +53,7 @@ def get_args():
 def train_mode(gen, dis):
 	####### Define optimizer #######
 	genOptimizer = optim.Adam(gen.parameters(), lr=opts.lr)
-	disOptimizer = optim.Adam(dis.parameters(), lr=opts.lr)
+	disOptimizer = optim.SGD(dis.parameters(), lr=opts.lr)
 
 	if gen.useCUDA:
 		torch.cuda.set_device(opts.gpuNo)
