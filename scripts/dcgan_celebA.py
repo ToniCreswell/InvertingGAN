e5424aaf5860c389e0989880a0b11d9bfc946d41
ToniCreswell <ac2211@ic.ac.uk>
2018-01-12 16:22:36 +0000

    use 0.5 momentum in G and D

--- scripts/dcgan_celebA.py
+++ scripts/dcgan_celebA.py
@@ -52,7 +52,7 @@ def get_args():
 
 def train_mode(gen, dis):
 	####### Define optimizer #######
-	genOptimizer = optim.RMSprop(gen.parameters(), lr=opts.lr, momentum=0.9)
+	genOptimizer = optim.RMSprop(gen.parameters(), lr=opts.lr, momentum=0.5)
 	disOptimizer = optim.RMSprop(dis.parameters(), lr=opts.lr, momentum=0.5)
 
 	if gen.useCUDA:
