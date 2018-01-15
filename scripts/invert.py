ac92a182ed558840b5fa8eb806677cc0a4fabcc4
ToniCreswell <ac2211@ic.ac.uk>
2018-01-15 19:13:59 +0000

    format change

--- scripts/invert.py
+++ scripts/invert.py
@@ -51,15 +51,15 @@ def get_args():
 def find_z(gen, x, nz, lr, exDir, maxEpochs=100):
 
 	#generator in eval mode
-	# gen.eval()
+	gen.eval()
 
 	if gen.useCUDA:
 		gen.cuda()
 
-	Zinit = Variable(torch.randn(1, nz).cuda(), requires_grad=True)
+	Zinit = Variable(torch.randn(1,opts.nz).cuda(), requires_grad=True)
 
 	#optimizer
-	optZ = torch.optim.RMSprop(params = [Zinit], lr=lr, momentum=0.9)
+	optZ = torch.optim.RMSprop([Zinit], lr=lr, momentum=0.9)
 
 	losses = {'rec': []}
 	for e in range(maxEpochs):
