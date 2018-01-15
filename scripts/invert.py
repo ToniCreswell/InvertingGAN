67534a2db0b92618668ee07d6a00546e3843f80e
ToniCreswell <ac2211@ic.ac.uk>
2018-01-15 19:41:12 +0000

    add the mean squared

--- scripts/invert.py
+++ scripts/invert.py
@@ -67,7 +67,7 @@ def find_z(gen, x, nz, lr, exDir, maxEpochs=100):
 		xHAT = gen.forward(Zinit)
 		loss = F.mse_loss(x, xHAT)
 
-		loss += 0 * Zinit.mean() #add to comp graph?
+		loss += Zinit.mean().sqr()
 
 		optZ.zero_grad()
 		loss.backward()
