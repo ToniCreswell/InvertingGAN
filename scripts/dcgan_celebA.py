62635c29f4b68a886150b5e73926d2421bb42916
ToniCreswell <ac2211@ic.ac.uk>
2018-01-17 12:56:04 +0000

    typo assigning beta1

--- scripts/dcgan_celebA.py
+++ scripts/dcgan_celebA.py
@@ -51,7 +51,7 @@ def get_args():
 	return parser.parse_args()
 
 
-def train_mode(gen, dis, useNoise=False, beta1=beta1):
+def train_mode(gen, dis, useNoise=False, beta1=0.5):
 	####### Define optimizer #######
 	genOptimizer = optim.Adam(gen.parameters(), lr=opts.lr, betas=(beta1, 0.999))
 	disOptimizer = optim.Adam(dis.parameters(), lr=opts.lr, betas=(beta1, 0.999))
