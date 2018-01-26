523a512871656aea32452d3fc2660f6671cbf057
ToniCreswell <ac2211@ic.ac.uk>
2018-01-26 11:01:02 +0000

    fix the transforms

--- scripts/dcgan_celebA.py
+++ scripts/dcgan_celebA.py
@@ -155,13 +155,13 @@ if __name__=='__main__':
 
 	####### Data set #######
 	print 'Prepare data loaders...'
-	transform = transforms.Compose([transforms.ToPILImage(), transforms.RandomHorizontalFlip(), transforms.ToTensor(),\
-	 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	transform = transforms.Compose([transforms.ToPILImage(), transforms.RandomHorizontalFlip(),\
+	 transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	trainDataset = CELEBA(root=opts.root, train=True, transform=transform)
 	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
 
-	transform = transforms.Compose([transforms.ToTensor(), \
-		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	transform = transforms.Compose([ transforms.ToPILImage(), \
+		transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	testDataset = CELEBA(root=opts.root, train=False, transform=transform)
 	testLoader = torch.utils.data.DataLoader(testDataset, batch_size=opts.batchSize, shuffle=False)
 	print 'Data loaders ready.'
