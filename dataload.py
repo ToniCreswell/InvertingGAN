6fcaa0facf1f7f8ed5fd5a40c7724d3dece08d67
ToniCreswell <ac2211@ic.ac.uk>
2018-01-26 10:47:11 +0000

    remove the fromarray in the dataload and send in the toPILimage as a transform instead

--- dataload.py
+++ dataload.py
@@ -163,7 +163,7 @@ class SHOES(data.Dataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img)
+        # img = Image.fromarray(img)
 
         if self.transform is not None:
             img = self.transform(img)
