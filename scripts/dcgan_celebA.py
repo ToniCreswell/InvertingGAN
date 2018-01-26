4eb98b276e64e93843bdc351d8dddc2fd7592766
ToniCreswell <ac2211@ic.ac.uk>
2018-01-26 10:53:48 +0000

    ***update dataloaders for both dcgans

--- scripts/dcgan_celebA.py
+++ scripts/dcgan_celebA.py
@@ -155,9 +155,9 @@ if __name__=='__main__':
 
 	####### Data set #######
 	print 'Prepare data loaders...'
-	transform = transforms.Compose([transforms.ToTensor(), \
-		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), transforms.RandomHorizontalFlip()])
-	trainDataset = CELEBA(root=opts.root, train=True, transform=transforms.ToTensor())
+	transform = transforms.Compose([transforms.ToPILImage(), transforms.RandomHorizontalFlip(), transforms.ToTensor(),\
+	 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	trainDataset = CELEBA(root=opts.root, train=True, transform=transform)
 	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
 
 	transform = transforms.Compose([transforms.ToTensor(), \
--- scripts/dcgan_shoes.py
+++ scripts/dcgan_shoes.py
@@ -159,9 +159,8 @@ if __name__=='__main__':
 	####### Data set #######
 	IM_SIZE = opts.imSize
 	print 'Prepare data loaders...'
-	transform = transforms.Compose([transforms.ToPILImage(), transforms.Resize((IM_SIZE, IM_SIZE)), \
-	 transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), \
-	 transforms.RandomHorizontalFlip()])
+	transform = transforms.Compose([transforms.ToPILImage(), transforms.Resize((IM_SIZE, IM_SIZE)), transforms.RandomHorizontalFlip(),\
+	 transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	trainDataset = SHOES(root=opts.root, train=True, transform=transform)
 	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
 
