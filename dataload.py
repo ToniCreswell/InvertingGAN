29429ba9f54ebc4a1f42518393098a293e1d2752
Toni Creswell <ac2211@ic.ac.uk>
2018-01-31 18:29:42 +0000

    added an omniglot data loader -- not tested yet

--- dataload.py
+++ dataload.py
@@ -180,4 +180,97 @@ class SHOES(data.Dataset):
             return len(self.test_data)
 
 
+class OMNI(data.Dataset): #omniglot
+    """
+    Args:
+        root (string): Root directory of dataset where directory
+            ``OMNI`` exists.
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        grain: 'None', 'fine', 'subfine'
+            None: Boots','Sandals', 'Shoes', 'Slippers'
+            fine: c. 20 sub categories
+            subfine: c.3000 subsub categories
+    """
+
+
+    def __init__(self, root, train=True, grain=None, transform=None, Ntest=1000):
+        
+
+        self.root = os.path.expanduser(root)
+        self.train = train  # training set or test set
+        self.filename='OMNI'
+        self.transform=transform
+        self.labels = ['Boots','Sandals', 'Shoes', 'Slippers']
+        self.grain = grain
+
+        ####### Shuffel data same way each time #######
+        
+        #shuffel data
+        np.random.seed(1993)
+        rndIdx = np.random.permutation(np.shape(xData)[0])
+        xData = xData[rndIdx]
+        yData = yData[rndIdx]
+
+        # now load the picked numpy arrays
+        if self.train:
+            xData = np.load(join(self.root, self.filename, 'omni_back_X.npy'), mmap_mode='r')
+            yData = np.load(join(self.root, self.filename, 'omni_back_Y.npy'), mmap_mode='r')
+            
+            np.random.seed(1993)
+            rndIdx = np.random.permutation(np.shape(xData)[0])
+            
+            self.train_data = xData[rndIdx]
+            self.train_labels = yData[rndIdx]
+            self.train_labels = self.train_labels.astype(int)
+            print np.shape(self.train_labels), np.shape(self.train_data)
+            print np.unique(self.train_labels)
+
+        else: #test
+            xData = np.load(join(self.root, self.filename, 'omni_eval_X.npy'), mmap_mode='r')
+            yData = np.load(join(self.root, self.filename, 'omni_eval_Y.npy'), mmap_mode='r')
+            
+            self.test_data = xData
+            self.test_labels = yData
+            self.test_labels = self.test_labels.astype(int)
+
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+
+        if self.train:
+            img, target = self.train_data[index], self.train_labels[index]
+        else:
+            img, target = self.test_data[index], self.test_labels[index]
+            
+
+        # doing this so that it is consistent with all other datasets
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        target = target.astype(int)
+
+
+        return img, target
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
+
+
 
