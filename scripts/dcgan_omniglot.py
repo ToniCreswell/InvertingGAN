bf181132ecc25d2f841ebb0eeb7dc544fe1255d6
Toni Creswell <ac2211@ic.ac.uk>
2018-01-31 18:52:53 +0000

    resize images

--- scripts/dcgan_omniglot.py
+++ scripts/dcgan_omniglot.py
@@ -156,11 +156,15 @@ if __name__=='__main__':
 
 	####### Data set #######
 	print 'Prepare data loaders...'
-	transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	transform = transforms.Compose([transforms.ToPILImage(), \
+		transforms.Resize((IM_SIZE, IM_SIZE)), transforms.RandomHorizontalFlip(),\
+		transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	trainDataset = OMNI(root=opts.root, train=True, transform=transform)
 	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
 
-	transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	transform = transforms.Compose([transforms.ToPILImage(), \
+		transforms.Resize((IM_SIZE, IM_SIZE)), transforms.ToTensor(), \
+		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 	testDataset = OMNI(root=opts.root, train=False, transform=transform)
 	testLoader = torch.utils.data.DataLoader(testDataset, batch_size=opts.batchSize, shuffle=False)
 	print 'Data loaders ready.'
