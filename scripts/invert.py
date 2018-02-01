22f5e251d04a43efed9954d5d8a252335db9d029
Toni Creswell <ac2211@ic.ac.uk>
2018-02-01 18:29:29 +0000

    save saying it's a batch

--- scripts/invert.py
+++ scripts/invert.py
@@ -99,7 +99,7 @@ def find_batch_z(gen, x, nz, lr, exDir, maxEpochs=100, alpha=1e-6, batchNo=0):
 	gen.eval()
 
 	#save the "original" images
-	save_image(x.data, join(exDir, 'original'+str(batchNo)+.png'), normalize=True)
+	save_image(x.data, join(exDir, 'original_batch'+str(batchNo)+.png'), normalize=True)
 
 	#Assume the prior is Standard Normal
 	pdf = torch.distributions.Normal(0, 1)
@@ -142,7 +142,7 @@ def find_batch_z(gen, x, nz, lr, exDir, maxEpochs=100, alpha=1e-6, batchNo=0):
 
 	#visualise the final output
 	xHAT = gen.forward(Zinit)
-	save_image(xHAT.data, join(exDir, 'rec'+str(batchNo)+'.png'), normalize=True)
+	save_image(xHAT.data, join(exDir, 'rec_batch'+str(batchNo)+'.png'), normalize=True)
 
 	return Zinit, recLoss.data[0]
 
