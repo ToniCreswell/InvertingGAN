fc88618ea85ec2a3002310a90bd046d1ddd98e22
Toni Creswell <ac2211@ic.ac.uk>
2018-10-11 18:34:10 +0100

    fix mean

--- scripts/invert.py
+++ scripts/invert.py
@@ -201,10 +201,10 @@ if __name__=='__main__':
 		allX.append(x.data) #incase the loader shuffles samples
 
 		if opts.oneBatch:
-			diff = (xRec.data - x.data)**2
+			diff = np.asarray((xRec.data - x.data)**2)
 			print('diff:', np.shape(diff))
 			print(diff)
-			mseLoss = np.mean((xRec.data.cpu() - x.data.cpu())**2, axis=(1,2,3))  # mean over colour channels and pixels
+			mseLoss = np.mean(diff, axis=(1,2,3))  # mean over colour channels and pixels
 			np.save(join(exDir, 'one_batch_mseLosses_per_sample.npy'), mseLoss)
 			meanLoss = np.mean(mseLoss) # mean over samples
 			stdLoss = np.std(mseLoss)  #std over samples
