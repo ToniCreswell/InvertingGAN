7b796a13743575d1c82243d9d29a51429c760a4e
Toni Creswell <ac2211@ic.ac.uk>
2018-10-30 17:43:04 +0000

    add imports

--- scripts/face_sum.py
+++ scripts/face_sum.py
@@ -5,8 +5,16 @@ from dataload import CELEBA_ALL_LABELS as CELEBA
 
 from invert import find_z, get_args
 
+import os
+from os.path import join
 
+import torch
 
+import numpy as np
+
+import matplotlib 
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 
 
 if __name__=='__main__':
@@ -26,14 +34,21 @@ if __name__=='__main__':
 
 	# Load data (glasses and male labels)
 	IM_SIZE = 64
-	print 'Prepare data loaders...'
+	print 'Prepare data loader...'
 	transform = transforms.Compose(
-		[transforms.ToPILImage(),transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+		[transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 	testDataset = CELEBA(root=opts.root, train=False, labels=['Male', 'Eyeglasses'], transform=transform, Ntest=1000)  #most models trained with Ntest=1000, but using 100 to prevent memory errors
-	gen = GEN(imSize=IM_SIZE, nz=opts.nz, fSize=opts.fSize)
+	testLoader = torch.utils.data.DataLoader(testDataset, batch_size=opts.batchSize, shuffle=False)
+	print 'Data loader ready.'
 
-	# Load model
+	# # Load model
+	# gen = GEN(imSize=IM_SIZE, nz=opts.nz, fSize=opts.fSize)
+	# if gen.useCUDA:
+	# 	torch.cuda.set_device(opts.gpuNo)
+	# 	gen.cuda()
+	# gen.load_params(opts.exDir, gpuNo=opts.gpuNo)
+	# print 'params loaded'
 
 
 	# Get men with glasses
