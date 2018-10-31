362c7a008e7b56b12f2f4a23915270ed5460c65a
Toni Creswell <ac2211@ic.ac.uk>
2018-10-31 13:07:58 +0000

    made face sum code more general

--- scripts/face_sum.py
+++ scripts/face_sum.py
@@ -36,10 +36,11 @@ if __name__=='__main__':
 	opts.data = 'CELEBA'
 	opts.imSize = 64
 	opts.numSamples = 20
+	opt.labels=['Male', 'Smiling']
 	# opts.batchSize = 100
 
 	#Create new subfolder for saving results and training params
-	exDir = join(opts.exDir, 'face_sum_experiments')
+	exDir = join(opts.exDir, ''+opts.labels[0]+'_'+opts.labels[1]+'_face_sum_experiments')
 	try:
 		os.mkdir(exDir)
 	except:
@@ -54,7 +55,7 @@ if __name__=='__main__':
 	transform = transforms.Compose(
 		[transforms.ToPILImage(), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-	testDataset = CELEBA(root=opts.root, train=False, labels=['Male', 'Eyeglasses'], transform=transform, Ntest=1000)  #most models trained with Ntest=1000, but using 100 to prevent memory errors
+	testDataset = CELEBA(root=opts.root, train=False, labels=opts.labels, transform=transform, Ntest=1000)  #most models trained with Ntest=1000, but using 100 to prevent memory errors
 	testLoader = torch.utils.data.DataLoader(testDataset, batch_size=opts.batchSize, shuffle=False)
 	print 'Data loader ready.'
 
@@ -73,30 +74,30 @@ if __name__=='__main__':
 	# Get men with glasses
 	idx_men_w_glasses = np.argwhere(torch.prod(y.data==torch.Tensor([1,1]).cuda(), dim=1))[0]
 	img_men_w_glasses = x[idx_men_w_glasses[:opts.numSamples]]
-	save_image(img_men_w_glasses.data, join(exDir,'img_men_w_glasses_original.png'), nrow=opts.numSamples, normalize=True)
+	save_image(img_men_w_glasses.data, join(exDir,'img_'+opts.labels[0]+'_w_'+opts.labels[1]+'_original.png'), nrow=opts.numSamples, normalize=True)
 
 
 	# Get men without glasses
 	idx_men_wout_glasses = np.argwhere(torch.prod(y.data==torch.Tensor([1,0]).cuda(), dim=1))[0]
 	img_men_wout_glasses = x[idx_men_wout_glasses[:opts.numSamples]]
-	save_image(img_men_wout_glasses.data, join(exDir,'img_men_wout_glasses_original.png'), nrow=opts.numSamples, normalize=True)
+	save_image(img_men_wout_glasses.data, join(exDir,'img_'+opts.labels[0]+'_wout_'+opts.labels[1]+'original.png'), nrow=opts.numSamples, normalize=True)
 
 
 	# Get womean without glasses
 	idx_women_wout_glasses = np.argwhere(torch.prod(y.data==torch.Tensor([0,0]).cuda(), dim=1))[0]
 	img_women_wout_glasses = x[idx_women_wout_glasses[:opts.numSamples]]
-	save_image(img_women_wout_glasses.data, join(exDir,'img_women_wout_glasses_original.png'), nrow=opts.numSamples, normalize=True)
+	save_image(img_women_wout_glasses.data, join(exDir,'img_not_'+opts.labels[0]+'_wout_'+opts.labels[1]+'original.png'), nrow=opts.numSamples, normalize=True)
 
 	# Put in one batch to make inversion faster:
 	x_in = torch.cat([img_men_w_glasses, img_men_wout_glasses, img_women_wout_glasses], dim=0)
 	print(np.shape(x_in))
 
 	try:
-		z_men_w_glasses = np.load(join(exDir, 'z_men_w_glasses.npy'))
+		z_men_w_glasses = np.load(join(exDir, 'z_'+opts.labels[0]+'_w_'+opts.labels[1]+'.npy'))
 		z_men_w_glasses = torch.Tensor(z_men_w_glasses).cuda()
-		z_men_wout_glasses = np.load(join(exDir, 'z_men_wout_glasses.npy'))
+		z_men_wout_glasses = np.load(join(exDir, 'z_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'))
 		z_men_wout_glasses = torch.Tensor(z_men_wout_glasses).cuda()
-		z_women_wout_glasses = np.load(join(exDir, 'z_women_wout_glasses.npy'))
+		z_women_wout_glasses = np.load(join(exDir, 'z_not_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'))
 		z_women_wout_glasses = torch.Tensor(z_women_wout_glasses).cuda()
 	except:
 		z_out = find_z(gen, x_in, nz=opts.nz, lr=opts.lr, exDir=exDir, maxEpochs=opts.maxEpochs)
@@ -105,14 +106,14 @@ if __name__=='__main__':
 		z_men_wout_glasses = z_out[opts.numSamples:2*opts.numSamples]
 		z_women_wout_glasses = z_out[2*opts.numSamples:]
 
-		np.save(join(exDir, 'z_men_w_glasses.npy'), z_men_w_glasses.detach().cpu().numpy())
-		np.save(join(exDir, 'z_men_wout_glasses.npy'), z_men_wout_glasses.detach().cpu().numpy())
-		np.save(join(exDir, 'z_women_wout_glasses.npy'), z_women_wout_glasses.detach().cpu().numpy())
+		np.save(join(exDir, 'z_'+opts.labels[0]+'_w_'+opts.labels[1]+'.npy'), z_men_w_glasses.detach().cpu().numpy())
+		np.save(join(exDir, 'z_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'), z_men_wout_glasses.detach().cpu().numpy())
+		np.save(join(exDir, 'z_not_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'), z_women_wout_glasses.detach().cpu().numpy())
 
 	# face sum NOT on means
 	z_woman_w_glasses = z_men_w_glasses - z_men_wout_glasses + z_women_wout_glasses
 	img_woman_w_glasses = gen.forward(z_woman_w_glasses)
-	save_image(img_woman_w_glasses, join(exDir,'img_woman_w_glasses.png'), nrow=opts.numSamples, normalize=True)
+	save_image(img_woman_w_glasses, join(exDir,'img_not_'+opts.labels[0]+'_w_'+opts.labels[1]+'.png'), nrow=opts.numSamples, normalize=True)
 
 	# face sum ON means
 	z_mean_man_w_glasses = torch.mean(z_men_w_glasses, dim=0, keepdim=True)
@@ -126,17 +127,10 @@ if __name__=='__main__':
 	img_mean_woman_wout_glasses = gen.forward(z_mean_woman_wout_glasses)
 	img_mean_woman_w_glasses = gen.forward(z_mean_woman_w_glasses)
 
-	save_image(img_mean_man_w_glasses, join(exDir,'img_mean_man_w_glasses.png'), nrow=1, normalize=True)
-	save_image(img_mean_man_wout_glasses, join(exDir,'img_mean_man_wout_glasses.png'), nrow=1, normalize=True)
-	save_image(img_mean_woman_wout_glasses, join(exDir,'img_mean_woman_wout_glasses.png'), nrow=1, normalize=True)
-	save_image(img_mean_woman_w_glasses, join(exDir,'img_mean_woman_w_glasses.png'), nrow=1, normalize=True)
-
-
-
-
-
-
-
+	save_image(img_mean_man_w_glasses, join(exDir,'img_mean'+opts.labels[0]+'_w_'+opts.labels[1]+'.png'), nrow=1, normalize=True)
+	save_image(img_mean_man_wout_glasses, join(exDir,'img_mean'+opts.labels[0]+'_wout_'+opts.labels[1]+'.png'), nrow=1, normalize=True)
+	save_image(img_mean_woman_wout_glasses, join(exDir,'img_mean_not_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.png'), nrow=1, normalize=True)
+	save_image(img_mean_woman_w_glasses, join(exDir,'img_mean_not_'+opts.labels[0]+'_w_'+opts.labels[1]+'.png'), nrow=1, normalize=True)
 
 
 
