dd3b4ba9c9b64debf3f5e1602bee8a458bd39eac
Toni Creswell <ac2211@ic.ac.uk>
2018-10-31 17:08:38 +0000

    num samples to 50

--- scripts/face_sum.py
+++ scripts/face_sum.py
@@ -35,7 +35,7 @@ if __name__=='__main__':
 	opts = get_args()
 	opts.data = 'CELEBA'
 	opts.imSize = 64
-	opts.numSamples = 100
+	opts.numSamples = 50
 	opts.labels=['Male', 'Eyeglasses']
 	# opts.batchSize = 100
 
