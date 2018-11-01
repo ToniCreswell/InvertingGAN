7b26e5d3455681a0f4db5c24ab7f9587f17580e4
Toni Creswell <ac2211@ic.ac.uk>
2018-11-01 16:41:01 +0000

    update glasses idx

--- scripts/face_sum.py
+++ scripts/face_sum.py
@@ -95,7 +95,7 @@ if __name__=='__main__':
 
 	try:
 		z_men_w_glasses = np.load(join(exDir, 'z_'+opts.labels[0]+'_w_'+opts.labels[1]+'.npy'))
-		z_men_w_glasses = torch.Tensor(z_men_w_glasses).cuda()[[0,2,6]]
+		z_men_w_glasses = torch.Tensor(z_men_w_glasses).cuda()[[0,2,6,14]]
 		z_men_wout_glasses = np.load(join(exDir, 'z_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'))
 		z_men_wout_glasses = torch.Tensor(z_men_wout_glasses).cuda()[[9,12,13,16,20,22,23,26,32,33]]
 		z_women_wout_glasses = np.load(join(exDir, 'z_not_'+opts.labels[0]+'_wout_'+opts.labels[1]+'.npy'))
