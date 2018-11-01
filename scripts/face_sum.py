4e670d877c774d23be417b17d4d3d2a9444b1881
Toni Creswell <ac2211@ic.ac.uk>
2018-11-01 15:46:44 +0000

    Male galsses

--- scripts/face_sum.py
+++ scripts/face_sum.py
@@ -36,7 +36,7 @@ if __name__=='__main__':
 	opts.data = 'CELEBA'
 	opts.imSize = 64
 	opts.numSamples = 10
-	opts.labels=['Smiling', 'Male']
+	opts.labels=['Male', 'Eyeglasses']
 	# opts.batchSize = 100
 
 	#Create new subfolder for saving results and training params
